15cb60764a7970185855ee75e0509a99062bff68
MaroM <maro.muszynski@gmail.com>
2019-03-27 14:41:23 +0100

    add customer service and its implementation

---
+++ spring5-mvc-rest/src/main/java/com/marom/spring5mvcrest/services/CustomerService.java
@@ -0,0 +1,12 @@
+package com.marom.spring5mvcrest.services;
+
+import com.marom.spring5mvcrest.api.model.CustomerDto;
+
+import java.util.List;
+
+public interface CustomerService {
+
+    List<CustomerDto> getAllCustomers();
+
+    CustomerDto getCustomerById(Long id);
+}
---
+++ spring5-mvc-rest/src/main/java/com/marom/spring5mvcrest/services/CustomerServiceImpl.java
@@ -0,0 +1,43 @@
+package com.marom.spring5mvcrest.services;
+
+import com.marom.spring5mvcrest.api.mapper.CustomerMapper;
+import com.marom.spring5mvcrest.api.model.CustomerDto;
+import com.marom.spring5mvcrest.repositories.CustomerRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+
+    private final CustomerMapper customerMapper;
+    private final CustomerRepository customerRepository;
+
+    public CustomerServiceImpl(CustomerMapper customerMapper, CustomerRepository customerRepository) {
+        this.customerMapper = customerMapper;
+        this.customerRepository = customerRepository;
+    }
+
+
+    @Override
+    public List<CustomerDto> getAllCustomers() {
+        return customerRepository
+                .findAll()
+                .stream()
+                .map(customer -> {
+                    CustomerDto customerDto = customerMapper.customerToCustomerDTO(customer);
+                    customerDto.setCustomerUrl("/api/v1/customers/" + customer.getId());
+                    return customerDto;
+                })
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public CustomerDto getCustomerById(Long id) {
+        return customerRepository.findById(id)
+                .map(customerMapper::customerToCustomerDTO)
+                .orElseThrow(RuntimeException::new); //todo implement better exception handling
+    }
+}
