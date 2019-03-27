29a0cef73445495ab9c42027b9af0da5ddf1be37
MaroM <maro.muszynski@gmail.com>
2019-03-27 16:54:17 +0100

    add customer mapper test

---
+++ spring5-mvc-rest/src/test/java/com/marom/spring5mvcrest/api/mapper/CustomerMapperTest.java
@@ -0,0 +1,32 @@
+package com.marom.spring5mvcrest.api.mapper;
+
+import com.marom.spring5mvcrest.api.model.CustomerDto;
+import com.marom.spring5mvcrest.domain.Customer;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CustomerMapperTest {
+
+    public static final String JIMMY = "Jimmy";
+    public static final String FALLON = "Fallon";
+
+    CustomerMapper customerMapper = CustomerMapper.INSTANCE;
+
+    @Test
+    public void customerToCustomerDTO() {
+        //given
+        Customer customer = new Customer();
+        customer.setFirstName(JIMMY);
+        customer.setLastName(FALLON);
+
+        //when
+        CustomerDto customerDto = customerMapper.customerToCustomerDTO(customer);
+
+        //then
+        assertEquals(JIMMY, customerDto.getFirstName());
+        assertEquals(FALLON, customerDto.getLastName());
+
+    }
+
+}
