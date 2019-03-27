2533f016fbb6eeda8a6453396ddbfefebbcc4a26
MaroM <maro.muszynski@gmail.com>
2019-03-27 08:42:25 +0100

    create Category repository

---
+++ spring5-mvc-rest/src/main/java/com/marom/spring5mvcrest/repositories/CategoryRepository.java
@@ -0,0 +1,7 @@
+package com.marom.spring5mvcrest.repositories;
+
+import com.marom.spring5mvcrest.domain.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
