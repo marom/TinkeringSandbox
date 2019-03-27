0d7c340324c51dd84112b9699f5483c26dfbf1d4
MaroM <maro.muszynski@gmail.com>
2019-03-27 14:25:09 +0100

    add Customer data transfer object

---
+++ spring5-mvc-rest/src/main/java/com/marom/spring5mvcrest/api/model/CustomerDto.java
@@ -0,0 +1,15 @@
+package com.marom.spring5mvcrest.api.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CustomerDto {
+
+    private String firstName;
+    private String lastName;
+    private String customerUrl;
+}
