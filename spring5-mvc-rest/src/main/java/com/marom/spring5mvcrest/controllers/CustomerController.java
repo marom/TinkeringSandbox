646a8ca58874f9a0452cb6c158a531982aa41931
MaroM <maro.muszynski@gmail.com>
2019-03-27 16:56:00 +0100

    add customer controller and test it

---
+++ spring5-mvc-rest/src/main/java/com/marom/spring5mvcrest/controllers/CustomerController.java
@@ -0,0 +1,36 @@
+package com.marom.spring5mvcrest.controllers;
+
+import com.marom.spring5mvcrest.api.model.CustomerDto;
+import com.marom.spring5mvcrest.api.model.CustomerListDto;
+import com.marom.spring5mvcrest.services.CustomerService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/customers/")
+public class CustomerController {
+
+    private final CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping
+    public ResponseEntity<CustomerListDto> getListOfCustomers(){
+
+        return new ResponseEntity<>(new CustomerListDto(customerService.getAllCustomers()),
+                HttpStatus.OK);
+
+    }
+
+    @GetMapping({"{id}"})
+    public ResponseEntity<CustomerDto> getCustomerById(@PathVariable Long id){
+        return new ResponseEntity<>(customerService.getCustomerById(id), HttpStatus.OK);
+    }
+
+}
---
+++ spring5-mvc-rest/src/test/java/com/marom/spring5mvcrest/controllers/CustomerControllerTest.java
@@ -0,0 +1,82 @@
+package com.marom.spring5mvcrest.controllers;
+
+import com.marom.spring5mvcrest.api.model.CustomerDto;
+import com.marom.spring5mvcrest.services.CustomerService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.Arrays;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class CustomerControllerTest {
+
+    @Mock
+    CustomerService customerService;
+
+    @InjectMocks
+    CustomerController customerController;
+
+    MockMvc mockMvc;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        mockMvc = MockMvcBuilders.standaloneSetup(customerController).build();
+    }
+
+    @Test
+    public void listCustomers() throws Exception {
+
+        //given
+        CustomerDto customer1 = new CustomerDto();
+        customer1.setFirstName("Michale");
+        customer1.setLastName("Weston");
+        customer1.setCustomerUrl("/api/customer/1");
+
+        CustomerDto customer2 = new CustomerDto();
+        customer2.setFirstName("Sam");
+        customer2.setLastName("Axe");
+        customer2.setCustomerUrl("/api/customer/2");
+
+        when(customerService.getAllCustomers()).thenReturn(Arrays.asList(customer1, customer2));
+
+        mockMvc.perform(get("/api/customers/")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.customers", hasSize(2)));
+    }
+
+    @Test
+    public void getCustomerById() throws Exception {
+
+        //given
+        CustomerDto customer1 = new CustomerDto();
+        customer1.setFirstName("Michale");
+        customer1.setLastName("Weston");
+        customer1.setCustomerUrl("/api/customer/1");
+
+        when(customerService.getCustomerById(anyLong())).thenReturn(customer1);
+
+        //when
+        mockMvc.perform(get("/api/customers/1")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.firstName", equalTo("Michale")));
+    }
+
+
+}
