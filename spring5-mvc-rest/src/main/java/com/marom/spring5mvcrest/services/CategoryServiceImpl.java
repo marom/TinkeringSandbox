a5d827ce39f3eb7b0e810bd6e2b1f9d178eafeff
MaroM <maro.muszynski@gmail.com>
2019-03-27 10:45:15 +0100

    add missing service annotation for category service implementation

--- spring5-mvc-rest/src/main/java/com/marom/spring5mvcrest/services/CategoryServiceImpl.java
+++ spring5-mvc-rest/src/main/java/com/marom/spring5mvcrest/services/CategoryServiceImpl.java
@@ -3,10 +3,12 @@ package com.marom.spring5mvcrest.services;
 import com.marom.spring5mvcrest.api.mapper.CategoryMapper;
 import com.marom.spring5mvcrest.api.model.CategoryDTO;
 import com.marom.spring5mvcrest.repositories.CategoryRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Service
 public class CategoryServiceImpl implements CategoryService {
 
 
