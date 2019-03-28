27f5906f42d301f4f25c987adaa5d2c353dbf32e
MaroM <maro.muszynski@gmail.com>
2019-03-28 12:47:20 +0100

    add Builder pattern to Vendor and VendorDto

--- spring5-mvc-rest/src/main/java/com/marom/spring5mvcrest/api/model/VendorDto.java
+++ spring5-mvc-rest/src/main/java/com/marom/spring5mvcrest/api/model/VendorDto.java
@@ -2,10 +2,12 @@ package com.marom.spring5mvcrest.api.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class VendorDto {
--- spring5-mvc-rest/src/main/java/com/marom/spring5mvcrest/domain/Vendor.java
+++ spring5-mvc-rest/src/main/java/com/marom/spring5mvcrest/domain/Vendor.java
@@ -1,6 +1,9 @@
 package com.marom.spring5mvcrest.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,6 +12,9 @@ import javax.persistence.Id;
 
 @Data
 @Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Vendor {
 
     @Id
