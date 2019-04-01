c3115dbde62707230a555bc8eeb7911f404a37c6
MaroM <maro.muszynski@gmail.com>
2019-04-01 09:40:10 +0200

    add API model property annotation

--- spring5-mvc-rest/src/main/java/com/marom/spring5mvcrest/api/model/VendorDto.java
+++ spring5-mvc-rest/src/main/java/com/marom/spring5mvcrest/api/model/VendorDto.java
@@ -1,6 +1,7 @@
 package com.marom.spring5mvcrest.api.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,6 +13,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class VendorDto {
 
+    @ApiModelProperty(value = "This is the name of the Vendor", required = true)
     private String name;
 
     @JsonProperty("vendor_url")
