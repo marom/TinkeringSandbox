74cf5ba0209b410abc35db1022f687f7d90df117
MaroM <maro.muszynski@gmail.com>
2019-04-01 15:57:46 +0200

    add Vendor repository

---
+++ spring5-webflux-rest/src/main/java/com/marom/spring5webfluxrest/repositories/VendorRepository.java
@@ -0,0 +1,7 @@
+package com.marom.spring5webfluxrest.repositories;
+
+import com.marom.spring5webfluxrest.domain.Vendor;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
+public interface VendorRepository extends ReactiveMongoRepository<Vendor, String> {
+}
