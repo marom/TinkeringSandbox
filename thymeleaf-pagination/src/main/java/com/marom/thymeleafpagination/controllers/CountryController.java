abbf45d1f1c24fd81e1ec8f6019bf04e0eafe66c
MaroM <maro.muszynski@gmail.com>
2019-04-04 12:57:22 +0200

    use Pageable as a request parameter

--- thymeleaf-pagination/src/main/java/com/marom/thymeleafpagination/controllers/CountryController.java
+++ thymeleaf-pagination/src/main/java/com/marom/thymeleafpagination/controllers/CountryController.java
@@ -2,6 +2,7 @@ package com.marom.thymeleafpagination.controllers;
 
 import com.marom.thymeleafpagination.repositories.CountryRepository;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,15 +17,22 @@ public class CountryController {
         this.countryRepository = countryRepository;
     }
 
-
     @GetMapping("/all")
     public String listAllCountries(Model model, @RequestParam(defaultValue = "0") int page) {
-        //showPage(@PageableDefault(size=4)Pageable pageable)
 
         model.addAttribute("countries", countryRepository.findAll(PageRequest.of(page, 8)));
         model.addAttribute("currentPage", page);
 
         return "allCountries";
+    }
+
+    @GetMapping("/all2")
+    public String listAllCountries(Model model, Pageable pageable) {
+
+        model.addAttribute("countries", countryRepository.findAll(pageable));
+        model.addAttribute("currentPage", pageable.getPageNumber());
+
+        return "allPageable";
     }
 
 }
