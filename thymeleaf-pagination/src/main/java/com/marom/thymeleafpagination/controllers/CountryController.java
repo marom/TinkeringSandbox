3280bb02766ee902768f51d8f3d9831d8ecdda36
MaroM <maro.muszynski@gmail.com>
2019-04-04 11:57:06 +0200

    add controller class for Country requests

---
+++ thymeleaf-pagination/src/main/java/com/marom/thymeleafpagination/controllers/CountryController.java
@@ -0,0 +1,29 @@
+package com.marom.thymeleafpagination.controllers;
+
+import com.marom.thymeleafpagination.repositories.CountryRepository;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class CountryController {
+
+    private CountryRepository countryRepository;
+
+    public CountryController(CountryRepository countryRepository) {
+        this.countryRepository = countryRepository;
+    }
+
+
+    @GetMapping("/all")
+    public String listAllCountries(Model model, @RequestParam(defaultValue = "0") int page) {
+        //showPage(@PageableDefault(size=4)Pageable pageable)
+
+        model.addAttribute("countries", countryRepository.findAll(PageRequest.of(page, 4)));
+
+        return "allCountries";
+    }
+
+}
