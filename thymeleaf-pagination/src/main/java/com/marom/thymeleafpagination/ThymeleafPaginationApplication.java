af2fc53de159522a0522a24c1e66b4ac16697236
MaroM <maro.muszynski@gmail.com>
2019-04-04 11:07:13 +0200

    initial commit for the project to show thymeleaf pagination

---
+++ thymeleaf-pagination/src/main/java/com/marom/thymeleafpagination/ThymeleafPaginationApplication.java
@@ -0,0 +1,13 @@
+package com.marom.thymeleafpagination;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafPaginationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafPaginationApplication.class, args);
+    }
+
+}
