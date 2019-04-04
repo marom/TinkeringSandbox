2b9fb7e95ace01de39e0346d42cd0e2992a0edb2
MaroM <maro.muszynski@gmail.com>
2019-04-04 12:03:32 +0200

    persist Country data into database

--- thymeleaf-pagination/src/main/java/com/marom/thymeleafpagination/DataLoader.java
+++ thymeleaf-pagination/src/main/java/com/marom/thymeleafpagination/DataLoader.java
@@ -1,5 +1,7 @@
 package com.marom.thymeleafpagination;
 
+import com.marom.thymeleafpagination.domain.Country;
+import com.marom.thymeleafpagination.repositories.CountryRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -7,6 +9,12 @@ import java.util.Locale;
 
 @Component
 public class DataLoader implements CommandLineRunner {
+
+    private CountryRepository countryRepository;
+
+    public DataLoader(CountryRepository countryRepository) {
+        this.countryRepository = countryRepository;
+    }
 
     @Override
     public void run(String... args) throws Exception {
@@ -17,7 +25,11 @@ public class DataLoader implements CommandLineRunner {
 
         for (Locale locale : Locale.getAvailableLocales()) {
             if ((locale.getDisplayCountry() != null) && (!"".equals(locale.getDisplayCountry()))) {
-                System.out.println(locale.getCountry() + "---" + locale.getDisplayCountry() + "---" + locale.getDisplayLanguage());
+                final Country country = new Country();
+                country.setCode(locale.getCountry());
+                country.setName(locale.getDisplayCountry());
+                country.setLanguage(locale.getDisplayLanguage());
+                countryRepository.save(country);
             }
         }
     }
