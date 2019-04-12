d28cdd3e64987006697ff029f089d747dcf39be8
MaroM <maro.muszynski@gmail.com>
2019-04-12 19:17:36 +0200

    add register component

--- gallery-frontend/src/app/app.module.ts
+++ gallery-frontend/src/app/app.module.ts
@@ -1,14 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home.component';
-import { PhotoListComponent } from './components/photo-list/photo-list.component';
-import { SidePanelComponent} from './components/side-panel/side-panel.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './components/home.component';
+import {PhotoListComponent} from './components/photo-list/photo-list.component';
+import {SidePanelComponent} from './components/side-panel/side-panel.component';
 
-import { PhotoService} from './services/photo.service';
-import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import {PhotoService} from './services/photo.service';
+import {NavBarComponent} from './components/nav-bar/nav-bar.component';
+import {RegisterComponent} from './components/register/register.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { NavBarComponent } from './components/nav-bar/nav-bar.component';
     HomeComponent,
     PhotoListComponent,
     SidePanelComponent,
-    NavBarComponent
+    NavBarComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
