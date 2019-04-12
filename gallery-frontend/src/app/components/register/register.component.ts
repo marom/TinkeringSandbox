2656d532954c7c0507955bebbb784e65c9124b52
MaroM <maro.muszynski@gmail.com>
2019-04-12 19:41:04 +0200

    add register component

--- gallery-frontend/src/app/components/register/register.component.ts
+++ gallery-frontend/src/app/components/register/register.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {User} from '../../models/user';
+import {RegisterService} from '../../services/register.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,23 @@ import {Component, OnInit} from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  newUser: User = new User();
+  registered: boolean = false;
+
+  constructor(private registerService : RegisterService) { }
 
   ngOnInit() {
   }
 
+  onSubmit() {
+    console.log("submit test");
+    this.registerService.sendUser(this.newUser)
+      .subscribe(
+        data => {
+          this.registered = true;
+          this.newUser = new User();
+        },
+        error => console.log(error)
+      );
+  }
 }
