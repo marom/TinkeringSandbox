15f8639ca51128df8a607373718dc895f6ed7e08
MaroM <maro.muszynski@gmail.com>
2019-04-12 18:59:24 +0200

    add routing module

--- gallery-frontend/src/app/app.module.ts
+++ gallery-frontend/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home.component';
 import { PhotoListComponent } from './components/photo-list/photo-list.component';
@@ -18,7 +19,8 @@ import { NavBarComponent } from './components/nav-bar/nav-bar.component';
     NavBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [
     PhotoService
