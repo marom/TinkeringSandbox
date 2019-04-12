1199bcf327018fce9df7e4454064e13dec9f1022
MaroM <maro.muszynski@gmail.com>
2019-04-12 17:13:09 +0200

    rename home folder to components

--- gallery-frontend/src/app/app.module.ts
+++ gallery-frontend/src/app/app.module.ts
@@ -2,8 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { PhotoListComponent } from './home/photo-list/photo-list.component';
+import { HomeComponent } from './components/home.component';
+import { PhotoListComponent } from './components/photo-list/photo-list.component';
 
 @NgModule({
   declarations: [
