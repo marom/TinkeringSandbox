684df994d708aaaffa4930219901ca956a8353bd
MaroM <maro.muszynski@gmail.com>
2019-04-12 19:03:59 +0200

    initial commit for gallery backend

---
+++ gallery-backend/src/test/java/com/marom/gallerybackend/GalleryBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.marom.gallerybackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GalleryBackendApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
