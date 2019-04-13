929d83a946f86d92f2aacd0735e5c10981b1a1bd
MaroM <maro.muszynski@gmail.com>
2019-04-13 07:58:29 +0200

    add user controller with findAll, save and findByUserName methods implementation

---
+++ gallery-backend/src/main/java/com/marom/gallerybackend/controllers/UserController.java
@@ -0,0 +1,26 @@
+package com.marom.gallerybackend.controllers;
+
+import com.marom.gallerybackend.model.User;
+import com.marom.gallerybackend.services.UserService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public String login() {
+        return "";
+    }
+
+    @PostMapping
+    public User register(@RequestBody User user) {
+        return userService.save(user);
+    }
+}
