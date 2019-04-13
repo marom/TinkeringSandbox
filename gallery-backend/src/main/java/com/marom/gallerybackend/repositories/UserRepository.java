e1894646d339ffa70cc824b7607af58daf268623
MaroM <maro.muszynski@gmail.com>
2019-04-13 09:25:07 +0200

    add missing repository annotation

--- gallery-backend/src/main/java/com/marom/gallerybackend/repositories/UserRepository.java
+++ gallery-backend/src/main/java/com/marom/gallerybackend/repositories/UserRepository.java
@@ -2,7 +2,9 @@ package com.marom.gallerybackend.repositories;
 
 import com.marom.gallerybackend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByUserName(String userName);
