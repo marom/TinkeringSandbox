d5fdb340e3d13e07da884219f8804db93b54b759
MaroM <maro.muszynski@gmail.com>
2019-04-14 19:42:59 +0200

    start implementing JWT lib

--- gallery-backend/src/main/java/com/marom/gallerybackend/GalleryBackendApplication.java
+++ gallery-backend/src/main/java/com/marom/gallerybackend/GalleryBackendApplication.java
@@ -1,10 +1,22 @@
 package com.marom.gallerybackend;
 
+import com.marom.gallerybackend.config.JwtFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GalleryBackendApplication {
+
+    @Bean
+    public FilterRegistrationBean jwtFilter() {
+        final FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+        registrationBean.setFilter(new JwtFilter());
+        registrationBean.addUrlPatterns("/api/*");
+
+        return registrationBean;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(GalleryBackendApplication.class, args);
