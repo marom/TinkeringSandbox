fe6e653052b356cdabda8d3907b82e87cdac0a09
MaroM <maro.muszynski@gmail.com>
2019-04-17 13:56:41 +0200

    add path to the login route

--- gallery-backend/src/main/java/com/marom/gallerybackend/controllers/UserController.java
+++ gallery-backend/src/main/java/com/marom/gallerybackend/controllers/UserController.java
@@ -2,7 +2,16 @@ package com.marom.gallerybackend.controllers;
 
 import com.marom.gallerybackend.model.User;
 import com.marom.gallerybackend.services.UserService;
-import org.springframework.web.bind.annotation.*;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.ServletException;
+import java.util.Date;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/user")
@@ -14,9 +23,30 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping
-    public String login() {
-        return "";
+    @PostMapping("/login")
+    public String login(@RequestBody Map<String, String> json) throws ServletException {
+
+        if (json.get("username") == null || json.get("password") == null) {
+            throw new ServletException("Please fill in username and password");
+        }
+
+        String userName = json.get("username");
+        String password = json.get("password");
+
+        User user = userService.findByUserName(userName);
+        if (user == null) {
+            throw new ServletException("User name not found.");
+        }
+
+        String pwd = user.getPassword();
+
+        if (!password.equals(pwd)) {
+            throw new ServletException("Invalid login. Please check your name and password");
+        }
+
+        String toReturn = Jwts.builder().setSubject(userName).claim("roles", "user").setIssuedAt(new Date())
+                .signWith(SignatureAlgorithm.HS256, "secretkey").compact();
+        return  toReturn;
     }
 
     @PostMapping("/register")
