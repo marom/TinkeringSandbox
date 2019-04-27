9f393819c52fa328e1a1d077e6ddfa239aaf1344
MaroM <maro.muszynski@gmail.com>
2019-04-27 08:01:42 +0200

    Proxy Design Pattern implementation

---
+++ design-patterns/src/structural/proxy/Internet.java
@@ -0,0 +1,6 @@
+package structural.proxy;
+
+public interface Internet {
+
+    void connectTo(String serverhost) throws Exception;
+}
---
+++ design-patterns/src/structural/proxy/Main.java
@@ -0,0 +1,14 @@
+package structural.proxy;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Internet internet = new ProxyInternet();
+        try {
+            internet.connectTo("google.com");
+            internet.connectTo("abc.com");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
