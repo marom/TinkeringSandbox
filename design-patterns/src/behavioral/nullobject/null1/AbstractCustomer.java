47332fb16802a9957cc881ba795b67bd2ea2b08f
MaroM <maro.muszynski@gmail.com>
2019-04-29 12:11:50 +0200

    Null Object Design Pattern implementation

---
+++ design-patterns/src/behavioral/nullobject/null1/AbstractCustomer.java
@@ -0,0 +1,8 @@
+package behavioral.nullobject.null1;
+
+public abstract class AbstractCustomer {
+
+    protected String name;
+    public abstract boolean isNil();
+    public abstract String getName();
+}
---
+++ design-patterns/src/behavioral/nullobject/null1/NullCustomer.java
@@ -0,0 +1,14 @@
+package behavioral.nullobject.null1;
+
+public class NullCustomer extends AbstractCustomer {
+
+    @Override
+    public String getName() {
+        return "Not Available in Customer Database";
+    }
+
+    @Override
+    public boolean isNil() {
+        return true;
+    }
+}
