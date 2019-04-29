33055b61d703329ee69379acc273d7478455e166
MaroM <maro.muszynski@gmail.com>
2019-04-29 12:29:15 +0200

    Another Null Object Design Pattern implementation

---
+++ design-patterns/src/behavioral/nullobject/null2/Node.java
@@ -0,0 +1,14 @@
+package behavioral.nullobject.null2;
+
+public interface Node {
+
+    String getName();
+
+    int getTreeSize();
+
+    Node getLeft();
+
+    Node getRight();
+
+    void walk();
+}
