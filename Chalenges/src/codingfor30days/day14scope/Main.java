d9dc294b87859a6f1122b477341407ef7652888d
marom <maro.muszynski@gmail.com>
2019-05-07 08:35:05 +0200

    day 14 of 30 days of coding exercise from HackerRank

---
+++ Chalenges/src/codingfor30days/day14scope/Main.java
@@ -0,0 +1,38 @@
+package codingfor30days.day14scope;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        sc.close();
+
+        Difference difference = new Difference(a);
+
+        difference.computeDifference();
+
+        System.out.print(difference.maximumDifference);
+    }
+}
+
+class Difference {
+    private int[] elements;
+    public int maximumDifference;
+
+    public Difference(int[] elements) {
+        this.elements = elements;
+    }
+
+    public void computeDifference() {
+
+        Arrays.sort(elements);
+        maximumDifference = Math.abs(elements[0] - elements[elements.length - 1]);
+    }
+}
