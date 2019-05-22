019f7c121da4671f970aa438a5f0467b9e4c4455
marom <maro.muszynski@gmail.com>
2019-05-22 15:21:50 +0100

    array rotation challenge

---
+++ Chalenges/src/arraysleftrotation/Main.java
@@ -0,0 +1,47 @@
+package arraysleftrotation;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    // Complete the rotLeft function below.
+    static int[] rotLeft(int[] a, int d) {
+        int[] result = new int[a.length];
+
+        for (int i = 0; i < a.length; i++) {
+            result[(i + (a.length - d)) % a.length] = a[i];
+        }
+            return result;
+
+    }
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+
+        String[] nd = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nd[0]);
+
+        int d = Integer.parseInt(nd[1]);
+
+        int[] a = new int[n];
+
+        String[] aItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int aItem = Integer.parseInt(aItems[i]);
+            a[i] = aItem;
+        }
+
+        int[] result = rotLeft(a, d);
+
+        System.out.println(result);
+        scanner.close();
+    }
+
+}
