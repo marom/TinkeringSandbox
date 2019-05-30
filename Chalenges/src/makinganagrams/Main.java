bc451b8e4ab9247c30c3f79b1f648f200348ac48
marom <maro.muszynski@gmail.com>
2019-05-30 11:41:09 +0100

    making anagrams challenge

--- Chalenges/src/makinganagrams/Main.java
+++ Chalenges/src/makinganagrams/Main.java
@@ -8,11 +8,28 @@ public class Main {
     // Complete the makeAnagram function below.
     static int makeAnagram(String a, String b) {
 
-        // sort both strings, check if equals
-        // remove one character from both
-        // sort check again
-        // repeat
+        // an array for every letter in alphabet
+        int[] letterCounts = new int[26];
 
+        // count a letter occurrence in the sting
+        for(char c : a.toCharArray()){
+            letterCounts[c-'a']++;
+        }
+
+        // remove count if a letter exist in string two
+        for(char c : b.toCharArray()){
+            letterCounts[c-'a']--;
+        }
+
+
+        //Ex. String: aaaab bbaaa
+        //Array: [1, -1, 0, 0, 0, ...etc]
+        // We need to remove 1 a and 1 b to make an anagram, so: Math.abs(1) + Math.abs(-1) == 2, which is the correct answer.
+        int result = 0;
+        for(int i : letterCounts){
+            result += Math.abs(i);
+        }
+        return result;
     }
 
     private static final Scanner scanner = new Scanner(System.in);
