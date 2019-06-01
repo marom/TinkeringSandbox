5eb710bec51a37ccc6bd92ac86b1272eb4e562a9
marom <maro.muszynski@gmail.com>
2019-06-01 08:43:20 +0100

    a very big sum challenge

---
+++ Chalenges/src/algorithms/averybigsum/Main.java
@@ -0,0 +1,41 @@
+package algorithms.averybigsum;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    // Complete the aVeryBigSum function below.
+    static long aVeryBigSum(long[] ar) {
+
+        long result = 0;
+        for (int i = 0; i < ar.length; i++) {
+            result = result + ar[i];
+        }
+        return result;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+
+        int arCount = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        long[] ar = new long[arCount];
+
+        String[] arItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < arCount; i++) {
+            long arItem = Long.parseLong(arItems[i]);
+            ar[i] = arItem;
+        }
+
+        long result = aVeryBigSum(ar);
+
+        System.out.println(result);
+
+        scanner.close();
+    }
+}
