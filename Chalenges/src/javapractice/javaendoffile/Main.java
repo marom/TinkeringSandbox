22578400f73cf7b7d9114cf82279c0586665f5ea
marom <maro.muszynski@gmail.com>
2019-06-03 17:07:57 +0100

    java End-Of-File challenge

---
+++ Chalenges/src/javapractice/javaendoffile/Main.java
@@ -0,0 +1,18 @@
+package javapractice.javaendoffile;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scan = new Scanner(System.in);
+        int i = 0;
+        while(scan.hasNext()){
+            i++;
+            System.out.println(i + " " + scan.nextLine());
+        }
+
+
+    }
+}
